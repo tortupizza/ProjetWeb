05cd43e7ae297943f2f6713a2e283dc97e4bcf08
tortupizza <tortupizza@DESKTOP-I4C3T6C>
2021-03-25 09:55:04 +0100

    ajout du selecteur pour la recherche

--- serv_projet/homepage/index.php
+++ serv_projet/homepage/index.php
@@ -15,9 +15,17 @@
 
     <div id="container">
 
-        <p>Entrer dans la barre de recherche le stage, domaine, ou l'entreprise rechercher</p>
+        <p>Entrer dans la barre de recherche le stage, l'entreprise ou l'utilisateur recherché</p>
         <form method="POST" action="search.php" id="my-form">
             <input class="form-control" type="text" placeholder="Search" aria-label="Search" style="width:50%;">
+              
+            <select name="type" id="typeselector">
+                <option value="Stage">Stage</option>
+                <option value="Entreprise">Entreprise</option>
+                <option value="Etudiant">Etudiant</option>
+                <option value="Pilote">Pilote</option>
+            </select>
+
         </form>
 
     </div>
